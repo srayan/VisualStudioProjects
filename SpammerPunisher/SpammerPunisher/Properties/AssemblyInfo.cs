b5cbed9d35e8cf1f851ed6b0b228ef131277541d
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2017-04-30 11:04:31 -0700

    Thought so!

--- SpammerPunisher/SpammerPunisher/Properties/AssemblyInfo.cs
+++ SpammerPunisher/SpammerPunisher/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("SpammerPunisher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("SpammerPunisher")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
