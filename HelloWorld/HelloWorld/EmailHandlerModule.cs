41058f89f1e0e0d19669c35fff4b51112ff04725
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2017-04-29 23:26:45 -0700

    Updating

---
+++ HelloWorld/HelloWorld/EmailHandlerModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using Evolution;
+
+namespace EmailHandlerModule
+{
+    class EmailHandlerModule
+    {
+        public static void EmailHandler()
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("yodeler999@gmail.com", "bhalobasha7425!");
+
+            MailMessage mm = new MailMessage("yodeler999@gmail.com", "finservb88@luckysouk.com", "You need to stop Spamming me!!", "I am tired of your emails. Stop spamming!");
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            client.Send(mm);
+            
+        }
+    }
+}
